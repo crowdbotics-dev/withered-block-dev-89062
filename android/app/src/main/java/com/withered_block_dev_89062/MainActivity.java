5719e207a4caf851b1a5dedac18fdf4527ccf4cc
Crowdbotics <engineering@crowdbotics.com>
2023-06-06 08:26:04 +0000

    Initial commit.

---
+++ android/app/src/main/java/com/withered_block_dev_89062/MainActivity.java
@@ -0,0 +1,15 @@
+package com.withered_block_dev_89062;
+
+import com.facebook.react.ReactActivity;
+
+public class MainActivity extends ReactActivity {
+
+  /**
+   * Returns the name of the main component registered from JavaScript. This is used to schedule
+   * rendering of the component.
+   */
+  @Override
+  protected String getMainComponentName() {
+    return "withered_block_dev_89062";
+  }
+}
---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "withered-block-dev-89062",
+  url: "https://withered-block-dev-89062.botics.co",
+  api: "https://withered-block-dev-89062.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
